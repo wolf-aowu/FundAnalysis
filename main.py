7ea4a3def117bd3550ba0bf38d3255c0b0f7adfb
wolf <1093731746@qq.com>
2025-11-28 20:22:41 +0800

    feat: 配置跨域资源共享，添加，实现前后端通信

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-
-# https: // blog.51cto.com/u_16099326/11296750
-# https: // fund.eastmoney.com/js/fundcode_search.js
-# https: // www.jianshu.com/p/d79d3cd62560
-# https://blog.csdn.net/zd1007129657/article/details/110429437
+from app.routers.root import root_router
+from app.configs.cors_config import origins
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(root_router, tags=["/"])
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
